e2046e5f382d578ac77c29c89e52f17dc2de0a90
tonydh <tonydigitalart@gmail.com>
2023-11-06 10:59:29 -0800

    optimizing js file

--- froyo-webapp.js
+++ froyo-webapp.js
@@ -1,37 +1,27 @@
-/* 
-    users prompted by HTML button for input
-    user input is strings separated by commas
-
-//  function that takes in a string as an argument
-    //  variable for object to hold a count for each flavor
-    //  loop to iterate thro array and add count to object
-        //  if a count doesnt exist, add it to object and count 1
-        //  if it does exist, increment the count of existing flavor
-    //  print to console + return the object
+/** 
+*  an HTML button prompts a user to start an order
+*  user input consists of flavor names separated by commas
+*  function takes in the input string and returns an object
 */
 
 function flavorCount(string) {
-    //  prompt user for input
-    let froyoChoices = prompt("Please enter a flavor of froyo for each order:");
+    //  prompt user for input and split flavors into an array
+    let froyoFlavors = prompt("Please enter a flavor of froyo for each order:").split(",");
 
     //  edge case if user cancels prompt input
-    if(froyoChoices === null) {
-        console.log("But.. froyo?");
+    if(froyoFlavors === null) {
         return 0;
     }
     
-    //  split the input string into an array
-    let eachFlavor = froyoChoices.split(",");
-
     //  declare an empty object for the froyo flavor count
     const froyoCount = {};
     
     //  loop thro the split array and send a count to empty object
-    for(let i = 0; i < eachFlavor.length; i++) {
+    for(let i = 0; i < froyoFlavors.length; i++) {
         //  check if the key exists already in the object
-        if(typeof froyoCount[eachFlavor[i]] === "undefined") {
-            froyoCount[eachFlavor[i]] = 1;
-        } else {froyoCount[eachFlavor[i]]++}
+        if(typeof froyoCount[froyoFlavors[i]] === "undefined") {
+            froyoCount[froyoFlavors[i]] = 1;
+        } else {froyoCount[froyoFlavors[i]]++}
     }
 
     console.log(froyoCount);
